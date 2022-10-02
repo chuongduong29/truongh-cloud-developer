6c6fbdcc4609914e4966487f672ef073c973e51f
Huynh Truong <chuongduong29@gmail.com>
2022-10-03 02:32:47 +0700

    TruongH: config to connect to port 3000

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-w6zihhyh.us.auth0.com',            // Auth0 domain
   clientId: 'cg6DMjH71rkK1OLSJF4chJtX4U5HRocN',          // Auth0 client id
-  callbackUrl: 'http://localhost:8080/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
